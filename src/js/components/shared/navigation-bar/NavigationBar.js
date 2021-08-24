4ffdb955d2a6ee6ff0093520f7d3543f9b18c620
SebinSong <songxevin@gmail.com>
2021-08-24 15:21:17 +1200

    add icon button to the navigation bar

--- src/js/components/shared/navigation-bar/NavigationBar.js
+++ src/js/components/shared/navigation-bar/NavigationBar.js
@@ -6,6 +6,7 @@ import logoBlackPath from '@images/logo_black.png'
 
 // child components
 import NavLinkItem from './nav-link-item/NavLinkItem'
+import IconButton from './icon-button/IconButton'
 
 import './NavigationBar.scss'
 
@@ -21,21 +22,26 @@ function NavigationBar (props) {
   return (
     <aside className="navigation-bar">
       <ul className="toolbar-container">
-        {/* logo */}
-        <li className="toolbar__item logo"
-          onClick={() => history.push('/home')}
-        >
-          <a>
-            <img className="toolbar__logo-image"
-              alt="tiny-shop-logo"
-              src={logoBlackPath} />
-          </a>
-        </li>
-
         <ul className="toolbar__nav-link-container">
+          {/* logo */}
+          <li className="toolbar__item logo"
+            onClick={() => history.push('/home')}
+          >
+            <a>
+              <img className="toolbar__logo-image"
+                alt="tiny-shop-logo"
+                src={logoBlackPath} />
+            </a>
+          </li>
+
           {navLinkList.map(
             item => <NavLinkItem key={item.id} {...item} />
           )}
+        </ul>
+
+        <ul className="toolbar__icon-btn-container">
+          <IconButton classes="search-icon">search</IconButton>
+          <IconButton classes="cart-icon">shopping_cart</IconButton>
         </ul>
       </ul>
     </aside>
--- src/js/components/shared/navigation-bar/icon-button/IconButton.js
+++ src/js/components/shared/navigation-bar/icon-button/IconButton.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+// child components
+import Icon from '@components/global/Icon'
+
 import './IconButton.scss'
 
-function IconButton () {
+function IconButton ({
+  children = '',
+  classes = ''
+}) {
   return (
-    <></>
+    <li className={`toolbar__item icon-button ${classes}`}>
+      <Icon tag="button"
+        classes="toolbar__icon-button__icon">{ children }</Icon>
+    </li>
   )
 }
 
